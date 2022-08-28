abb7e1d85bf79c1a7cdb49c4025273eda7dbe288
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-28 13:22:22 +0300

    Complite task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryMarkup = galleryItems
+  .map(
+    item => `<a class="gallery__item" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      title="${item.description}"
+    />
+  </a>
+</div>`
+  )
+  .join('');
+
+galleryRef.insertAdjacentHTML('beforeend', createGalleryMarkup);
+
+galleryRef.addEventListener('click', event => {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.on('show.simplelightbox', function () {
+    gallery.captionDelay = 250;
+  });
+});
 
 console.log(galleryItems);
