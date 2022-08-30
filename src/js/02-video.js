bc22498a42e0e43f2b769f6dd66421496df77175
Stanislav Pylypeichenko <pylypeichenko.st@gmail.com>
2022-08-30 14:29:00 +0300

    Add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const playerCurrentTime = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  localStorage.setItem(playerCurrentTime, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(playerCurrentTime);
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
